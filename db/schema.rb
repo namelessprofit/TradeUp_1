c9a1c96673dd6b5161c92c1dcea5ca414930c3d1
Jesser360 <jesser360@gmail.com>
2017-04-26 16:37:25 -0700

    created allll the models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426232915) do
+ActiveRecord::Schema.define(version: 20170426233444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_favorites_on_item_id", using: :btree
+    t.index ["user_id"], name: "index_favorites_on_user_id", using: :btree
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -57,5 +66,7 @@ ActiveRecord::Schema.define(version: 20170426232915) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "favorites", "items"
+  add_foreign_key "favorites", "users"
   add_foreign_key "items", "users"
 end
