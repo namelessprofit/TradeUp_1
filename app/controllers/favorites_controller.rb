374ed19d26e01a1a45e8fcbfe98d70f45831c9ce
Archy Posada <posadaarchy@gmail.com>
2017-04-26 18:32:47 -0700

    added controller logic for offers and favorites. also creted routes for both

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,63 @@
 class FavoritesController < ApplicationController
+    def index
+      @favorites = Favorites.all
+    end
+
+    def new
+      @favorite = Favorite.new
+    end
+
+    def create
+      favorite = Favorite.new(favorite_params)
+
+      if favorite.save
+        redirect_to favorite_path(favorite)
+      else
+        flash[:error] = favorite.errors.full_messages.join(", ")
+        redirect_to new_favorite_path
+      end
+    end
+
+    def show
+      favorite_id = params[:id]
+
+      @favorite = Favorite.find_by_id(favorite_id)
+
+    end
+
+    def edit
+      favorite_id = params[:id]
+
+      @favorite = Favorite.find_by_id(favorite_id)
+
+    end
+
+    def update
+      favorite_id = params[:id]
+
+      favorite = Favorite.find_by_id(favorite_id)
+
+      favorite.update_attributes(favorite_params)
+
+      redirect_to favorite_path(favorite)
+    end
+
+    def destroy
+      favorite_id = params[:id]
+
+      favorite = Favorite.find_by_id(favorite_id)
+
+      favorite.destroy
+
+      redirect_to favorite_path
+    end
+
+    private
+
+    def favorite_params
+      params.require(:favorite).permit(:name, :description)
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "creatures#index"
+
+  get "/favorites", to: "favorites#index", as: "favorites"
+  get "/favorites/new", to: "favorites#new", as: "new_favorite"
+  post "/favorites", to: "favorites#create"
+  get "/favorites/:id", to: "favorites#show", as: "favorite"
+  get "/favorites/:id/edit", to: "favorites#edit", as: "edit_favorite"
+  patch "/favorites/:id", to: "favorites#update"
+  delete "/favorites/:id", to: "favorites#destroy"
+end
+
+  get "/offers", to: "offers#index", as: "offers"
+  get "/offers/new", to: "offers#new", as: "new_offer"
+  post "/offers", to: "offers#create"
+  get "/offers/:id", to: "offers#show", as: "offer"
+  get "/offers/:id/edit", to: "offers#edit", as: "edit_offer"
+  patch "/offers/:id", to: "offers#update"
+  delete "/offers/:id", to: "offers#destroy"
 end
