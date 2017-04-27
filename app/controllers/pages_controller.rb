c57d1e68b5b40f644813246a45744a04496ce330
Megan Wiley <mwiley322@gmail.com>
2017-04-27 14:25:13 -0700

    Make random images appear from Items list on home page under featured items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,17 @@
 class PagesController < ApplicationController
   def home
     if session[:user_id]
-    @user = User.find(session[:user_id])
+      @user = User.find(session[:user_id])
     else
       @user = nil
     end
+    @items = Item.all
+    @itemcount = Item.count
   end
 
   def learnmore
     if session[:user_id]
-    @user = User.find(session[:user_id])
+      @user = User.find(session[:user_id])
     else
       @user = nil
     end
