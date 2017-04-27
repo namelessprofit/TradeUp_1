985f314da9dd5d7da8586b248d66a7b6c3e9bf2f
Megan Wiley <mwiley322@gmail.com>
2017-04-27 13:46:51 -0700

    Add learn more page and additional home page styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,17 @@ class PagesController < ApplicationController
   def home
     if session[:user_id]
     @user = User.find(session[:user_id])
-  else
-    @user = nil
+    else
+      @user = nil
+    end
   end
-end
+
+  def learnmore
+    if session[:user_id]
+    @user = User.find(session[:user_id])
+    else
+      @user = nil
+    end
+  end
+
 end
