c0dc8652d8db397389f02b3610c72921cb27dc87
Archy Posada <posadaarchy@gmail.com>
2017-05-01 10:52:18 -0700

    added about us page and routes, fixed styles in application, pages, users, category, contacts, learnmore, home, show, footer,

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/learnmore', to: 'pages#learnmore', as: 'learnmore_path'
   get '/contacts', to: 'pages#contacts'
+  get '/about', to: 'pages#about'
 
   get '/items', to: 'items#index', as: 'items_path'
   get '/items/edit/:id', to: 'items#edit', as: 'edit_item_path'
