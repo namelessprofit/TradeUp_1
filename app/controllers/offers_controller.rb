934d979412cea28bd51d47344fc2e240136935bf
Jesser360 <jesser360@gmail.com>
2017-05-02 13:33:10 -0700

    bugz

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -59,11 +59,13 @@ class OffersController < ApplicationController
             @myItem.user = @temp
             @myItem.save
             @theirItem.save
-            @offer.save
             @myOld = Offer.where(initiator_id: current_user.id.to_s).where(offered_item_id: @offer.requested_item_id)
             @otherOld = Offer.where(requested_item_id: @offer.requested_item_id).where(is_completed: false)
-            @otherOld.delete_all
-            @myOld.delete_all
+            @alsoOld = Offer.where(initiator_id: current_user.id.to_s).where(offered_item_id: @offer.offered_item_id)
+            @otherOld.destroy
+            @myOld.destroy
+            @alsoOld.destroy
+            @offer.save
             flash[:success] = "Trade was a success! Connect with the user to pick up your item."
             redirect_to user_path_url(@user)
           else
