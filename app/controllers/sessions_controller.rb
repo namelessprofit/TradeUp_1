d57c33f5c5fe5ace277a1bec526bbd754d8edceb
Megan Wiley <mwiley322@gmail.com>
2017-05-02 11:49:50 -0700

    Small edits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@
     def create
       @user = User.find_by(email: params[:session][:email])
       if @user && @user.authenticate(params[:session][:password])
+        session[:user_id]= @user.id
         redirect_to user_path_url(@user)
       else
         flash[:error]= "An error has occurred. Please try again."
